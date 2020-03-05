3096186bdfd0dce69b0fa3a57bfa28aba7b18f40
kresnaputra <kresnaputra05@gmail.com>
2020-03-05 15:58:21 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text>it is app</Text>
+    </NavigationContainer>
+  );
+}
